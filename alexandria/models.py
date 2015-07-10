da4f890e84d3033c9e06bee273d782c284ee3fae
uggla <rene.ribaud@free.fr>
2015-07-10 14:27:20 +0200

    Make reference models attribute private. Create a get_model method to get a copy of reference data model.

--- alexandria/models.py
+++ alexandria/models.py
@@ -23,7 +23,8 @@ class Model(object):
             # Derive attribute name from file.
             attr_name = os.path.basename(file)
             attr_name = re.sub(r"\..*$", "", attr_name)
-
+            attr_name = "__" + attr_name # Make it private 
+            
             # read json file
             with open(file) as json_data:
                 data_structure = json.load(json_data)
@@ -33,3 +34,11 @@ class Model(object):
             setattr(self, attr_name, data_structure)
             self.reference_items.append(attr_name)
         return True
+
+    def get_model(self,model_name):
+        if not model_name[0:1] == "__":
+            model_name = "__" + model_name
+        model = getattr(self, model_name)
+        return model.copy()
+        
+        
